1d9b7ac5a4cd12b8eeb6b087e92a488e7a94957b
Sunday Mgbogu <digitalsimboja@gmail.com>
2022-09-07 16:09:34 +0100

    Completed the frontend with NFT Listing, creating, updating, buying and cancelling options

---
+++ frontend/constants.js
@@ -0,0 +1,4 @@
+export const SUBGRAPH_URL = 
+"https://api.thegraph.com/subgraphs/name/digitalsimboja/celo-nft-marketplace"
+
+export const MARKETPLACE_ADDRESS = "0x6f43e6700406a4231a488987B9dB1827FE187cF4"
